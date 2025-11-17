f778b3f5fd8d3a4adf86bd73328e9146bcfa77bb
2303a51585 <2303a51585@sru.edu.in>
2025-11-17 21:44:43 +0530

    Update test_withdraw.py

--- test_withdraw.py
+++ test_withdraw.py
@@ -6,9 +6,11 @@ def test_withdraw_success():
     acc.withdraw(400)
     assert acc.balance == 600
 
+
 def test_withdraw_insufficient_balance():
     acc = CurrentAccount(acc_number="C200", holder="Sam", balance=300)
 
+    # withdraw(500) should raise "Insufficient balance"
     with pytest.raises(Exception) as exc:
         acc.withdraw(500)
 
